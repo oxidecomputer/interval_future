80712bb80410acde3ef38cb71ab7de0930adccf9
Sean Klein <sean@oxide.computer>
2021-03-11 12:30:51 -0500

    Expose api as function

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ use std::{
 };
 
 /// Implements a future, using a caller-supplied polling function.
-pub struct IntervalFuture<O, F>
+struct IntervalFuture<O, F>
 where
     F: FnMut() -> Poll<O> + Unpin,
 {
@@ -22,78 +22,75 @@ where
     waker: Option<Waker>,
 }
 
-impl<O, F> IntervalFuture<O, F>
+/// Creates a new future which wraps a synchronous function `f`
+/// that may be called repeatedly on `interval_period` until
+/// it completes.
+///
+/// Generally, when using futures, an explicit waker should
+/// be used to cause re-polling on a more precise basis, but that
+/// isn't always available. This method allows callers to create
+/// a future which will automatically be-checked with a specified
+/// regularity.
+///
+/// # Arguments
+/// - `f`: A function to be polled. Returns [`Poll::Ready`] with
+/// a result to complete the future, and [`Poll::Pending`] otherwise.
+/// `f` will not be re-invoked after completing.
+/// - `interval_period`: A duration of time to wait before
+/// re-invoking `f`. This is a *suggestion*, as the function may
+/// actually be re-polled more frequently.
+///
+/// # Example
+///
+/// ```
+/// use std::task::Poll;
+/// use std::time::{Duration, Instant};
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let time_to_complete = Duration::from_secs(1);
+///     let interval = Duration::from_millis(200);
+///     let timeout = Duration::from_secs(2);
+///
+///     let poll_start = Instant::now();
+///     let f = || {
+///         let elapsed = Instant::now().duration_since(poll_start);
+///         if elapsed > time_to_complete {
+///             println!("Ready");
+///             Poll::Ready(5)
+///         } else {
+///             println!("Not ready after {} ms", elapsed.as_millis());
+///             Poll::Pending
+///         }
+///     };
+///
+///     let fut = interval_future::to_future(f, interval);
+///     let val = tokio::time::timeout(timeout, fut).await.unwrap();
+///     println!("Got my value: {}", val);
+/// }
+/// ```
+pub async fn to_future<O, F>(f: F, interval_period: Duration) -> O
 where
     F: FnMut() -> Poll<O> + Unpin,
 {
-    /// Creates a new future which wraps a synchronous function `f`
-    /// that may be called repeatedly on `interval_period` until
-    /// it completes.
-    ///
-    /// Generally, when using futures, an explicit waker should
-    /// be used to cause re-polling on a more precise basis, but that
-    /// isn't always available. This method allows callers to create
-    /// a future which will automatically be-checked with a specified
-    /// regularity.
-    ///
-    /// # Arguments
-    /// - `f'`: A function to be polled. Returns [`Poll::Ready`] with
-    /// a result to complete the future, and [`Poll::Pending`] otherwise.
-    /// `f` will not be re-invoked after completing.
-    /// - `interval_period`: A duration of time to wait before
-    /// re-invoking `f`. This is a *suggestion*, as the function may
-    /// actually be re-polled more frequently.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use interval_future::IntervalFuture;
-    /// use std::task::Poll;
-    /// use std::time::{Duration, Instant};
-    ///
-    /// #[tokio::main]
-    /// async fn main() {
-    ///     let time_to_complete = Duration::from_secs(1);
-    ///     let interval = Duration::from_millis(200);
-    ///     let timeout = Duration::from_secs(2);
-    ///
-    ///     let poll_start = Instant::now();
-    ///     let f = || {
-    ///         let elapsed = Instant::now().duration_since(poll_start);
-    ///         if elapsed > time_to_complete {
-    ///             println!("Ready");
-    ///             Poll::Ready(5)
-    ///         } else {
-    ///             println!("Not ready after {} ms", elapsed.as_millis());
-    ///             Poll::Pending
-    ///         }
-    ///     };
-    ///
-    ///     let fut = IntervalFuture::wrap(f, interval);
-    ///     let val = tokio::time::timeout(timeout, fut).await.unwrap();
-    ///     println!("Got my value: {}", val);
-    /// }
-    /// ```
-    pub async fn wrap(f: F, interval_period: Duration) -> O {
-        let fut = IntervalFuture {
-            f,
-            completed: false,
-            waker: None,
-        };
-        pin_mut!(fut);
+    let fut = IntervalFuture {
+        f,
+        completed: false,
+        waker: None,
+    };
+    pin_mut!(fut);
 
-        let mut interval_fut = tokio::time::interval(interval_period);
+    let mut interval_fut = tokio::time::interval(interval_period);
 
-        // First tick completes immediately.
-        interval_fut.tick().await;
-        loop {
-            select! {
-                o = fut => return o,
-                _ = interval_fut.tick().fuse() => (),
-            }
-            if let Some(waker) = fut.waker.take() {
-                waker.wake();
-            }
+    // First tick completes immediately.
+    interval_fut.tick().await;
+    loop {
+        select! {
+            o = fut => return o,
+            _ = interval_fut.tick().fuse() => (),
+        }
+        if let Some(waker) = fut.waker.take() {
+            waker.wake();
         }
     }
 }
